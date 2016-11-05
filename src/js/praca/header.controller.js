fc0dc0cb6310077948bc1ddb7d5fb61b6d70ecb2
André  "decko" de Brito <decko@birosca.mobi>
2016-11-05 09:21:25 -0200

    WIP: Adiciona o suporte a envio da imagem de cabeçalho das paginas das Praças.

--- src/js/praca/header.controller.js
+++ src/js/praca/header.controller.js
@@ -1,36 +1,38 @@
 class ChangeHeaderImgCtrl {
-	constructor($scope, $timeout, $mdDialog, Upload, AppConstants) {
-		'ngInject';
+  constructor($scope, $timeout, $mdDialog, $log, Upload, AppConstants) {
+    "ngInject";
 
-		this._$mdDialog = $mdDialog;
-		this._$scope = $scope;
-		this._AppConstants = AppConstants;
-		this._Upload = Upload;
-		this._praca = $scope.praca;
-		this._$timeout = $timeout;
-		self = this;
-	}
+    this._$mdDialog = $mdDialog;
+    this._$scope = $scope;
+    this._AppConstants = AppConstants;
+    this._Upload = Upload;
+    this._praca = $scope.praca;
+    this._$timeout = $timeout;
+    this._$log = $log;
+  }
 
-	cancel() {
-		this._$mdDialog.cancel();
-	}
+  cancel() {
+    this._$mdDialog.cancel();
+  }
 
-	upload(dataUrl, name) {
-		this._Upload.upload({
-			url: this._AppConstants.api + '/pracas/' + this._praca.id_pub + '/header_upload/',
-			data: { header_img: this._Upload.dataUrltoBlob(dataUrl, name) }, })
-			.then(
-				function(response) {
-					self._$timeout(function() {
-						$scope.result = response.data;
-					});
-				}, function(response) {
-					if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-				}, function (evt) {
-					$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-				}
-			);
-	}
+  upload(dataUrl, name) {
+    this._Upload.upload({
+      url: `${this._AppConstants.api}/pracas/${this._praca.id_pub}/header_upload/`,
+      method: "POST",
+      data: { header_img: this._Upload.dataUrltoBlob(dataUrl, name) },
+    })
+      .then(
+        (response) => this._$timeout(() => this._$scope.result = response.data),
+        (err) => { if (err.status > 0) { this._$scope.errorMsg = `${err.status}: ${err.data}` } },
+        (evt) => this._$scope.progress = parseInt(100.0 * evt.loaded / evt.total),
+      )
+      .then(
+        response => {
+          this._$log.log(response.data);
+          this._praca.header_url = response.data.header_url;
+        }
+      );
+  }
 
 
 
