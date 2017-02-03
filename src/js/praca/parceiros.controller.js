a229f774b5993354e1e23f39dd64e41730b300fc
André  "decko" de Brito <decko@birosca.mobi>
2017-02-03 16:41:17 -0200

    Corrige o alinhamento de icones e altera a lista de atuação dos parceiros.

--- src/js/praca/parceiros.controller.js
+++ src/js/praca/parceiros.controller.js
@@ -12,87 +12,51 @@ class ParceirosCtrl {
     this._listaAtividades = [
       {
         value: 1,
-        display_name: "agricultura, pecuária, produção florestal, pesca e aqüicultura",
+        display_name: "agropecuária",
       },
       {
         value: 2,
-        display_name: "indústrias extrativas",
+        display_name: "assistência social",
       },
       {
         value: 3,
-        display_name: "indústrias de transformação",
+        display_name: "comércio",
       },
       {
         value: 4,
-        display_name: "eletricidade e gás",
+        display_name: "comunicação",
       },
       {
         value: 5,
-        display_name: "água, esgoto, atividades de gestão de resíduos e descontaminação",
+        display_name: "cultura",
       },
       {
-        value: 6,
-        display_name: "construção",
-      },
-      {
-        value: 7,
-        display_name: "comércio; reparação de veículos automotores e motocicletas",
-      },
-      {
-        value: 8,
-        display_name: "transporte, armazenagem e correio",
-      },
-      {
-        value: 9,
-        display_name: "alojamento e alimentação",
-      },
-      {
-        value: 10,
-        display_name: "informação e comunicação",
-      },
-      {
-        value: 11,
-        display_name: "atividades financeiras, de seguros e serviços relacionados",
-      },
-      {
-        value: 12,
-        display_name: "atividades imobiliárias",
-      },
-      {
-        value: 13,
-        display_name: "atividades profissionais, científicas e técnicas",
-      },
-      {
-        value: 14,
-        display_name: "atividades administrativas e serviços complementares",
-      },
-      {
-        value: 15,
-        display_name: "administração pública, defesa e seguridade social",
-      },
-      {
-        value: 16,
+        value: 5,
         display_name: "educação",
       },
       {
-        value: 17,
-        display_name: "saúde humana e serviços sociais",
+        value: 5,
+        display_name: "esporte",
       },
       {
-        value: 18,
-        display_name: "artes, cultura, esporte e recreação",
+        value: 6,
+        display_name: "indústria",
       },
       {
-        value: 19,
-        display_name: "outras atividades de serviços",
+        value: 7,
+        display_name: "organização comunitária",
       },
       {
-        value: 20,
-        display_name: "serviços domésticos",
+        value: 8,
+        display_name: "organização social",
       },
       {
-        value: 21,
-        display_name: "organismos internacionais e outras instituições extraterritoriais",
+        value: 9,
+        display_name: "saúde",
+      },
+      {
+        value: 10,
+        display_name: "serviços",
       },
     ];
 
