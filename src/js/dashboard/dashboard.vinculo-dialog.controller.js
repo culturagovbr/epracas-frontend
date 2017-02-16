e01aebd9943a94f80f3f4bc6da42252529af75f0
Yurick <yurick@flama.me>
2017-02-16 21:46:11 -0200

    Adds zoomable-image at its rightful place

--- src/js/dashboard/dashboard.vinculo-dialog.controller.js
+++ src/js/dashboard/dashboard.vinculo-dialog.controller.js
@@ -6,10 +6,14 @@ class DashboardVinculoDialogCtrl {
     this._$mdDialog = $mdDialog;
 
     $scope.pedido = pedido;
+
     $scope.vincFiles = {
       comp: pedido.files.filter(file => file.tipo == 'compFile')[0],
       cpf: pedido.files.filter(file => file.tipo == 'cpfFile')[0]
     }
+
+    $scope.vincFiles.comp.url = "http://sossolteiros.bol.uol.com.br/wp-content/uploads/2013/07/Jack-Nicholson.jpg"
+    $scope.vincFiles.cpf.url = "http://sossolteiros.bol.uol.com.br/wp-content/uploads/2013/07/Jack-Nicholson.jpg"
   }
 }
 
