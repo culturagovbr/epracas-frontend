66824d82906b905282b218bce6968b042a88cf33
André  "decko" de Brito <decko@birosca.mobi>
2017-02-23 14:40:55 -0300

    WIP: Busca exibir o mapa na tela de geolocalização.

--- src/js/home/geoloc.controller.js
+++ src/js/home/geoloc.controller.js
@@ -1,5 +1,5 @@
 class GeolocCtrl {
-  constructor($scope, $state, GeoLocation) {
+  constructor($scope, $state, GeoLocation, leafletData) {
     "ngInject";
 
     $scope.$state = $state;
@@ -16,25 +16,53 @@ class GeolocCtrl {
         dragging: false,
       },
       markers: {},
+      center: {
+        lat: Number(40.712),
+        lng: Number(-74.125),
+        zoom: 1,
+      },
     }
 
     this.pracas_proximas = [];
+    let bounds = [];
 
     GeoLocation.getCurrentPosition()
-    .then(position => GeoLocation.getDistanceList(position))
-    .then(distances => distances.forEach((praca, index) => {
-      this.pracas_proximas.push(praca)
+      .then(position => GeoLocation.getDistanceList(position))
+      .then(distances => distances.forEach((praca, index) => {
+        this.pracas_proximas.push(praca);
 
-      let pracaName = praca.nome.split("-")[0]
-      this.geoLoc.markers[pracaName] = {
-        lat: parseFloat(praca.latlong.split(', ')[0]),
-        lng: parseFloat(praca.latlong.split(', ')[1]),
-        message: praca.nome,
-        draggable: false,
-        focus: false
+        let pracaId = praca.id_pub.split("-")[0];
+        this.geoLoc.markers[pracaId] = {
+          lat: parseFloat(praca.latlong.split(', ')[0]),
+          lng: parseFloat(praca.latlong.split(', ')[1]),
+          message: praca.nome,
+          draggable: false,
+          focus: false
+        }
+
+        const marker = L.marker(praca.latlong.split(',')[0], praca.latlong.split(',')[1]);
+        bounds.push(marker);
       }
-    }))
-    .catch(error => console.warn(error))
+      )
+      )
+      .catch(error => console.warn(error))
+
+
+    const pontos = new L.featureGroup(bounds);
+    const fronteira = pontos.getBounds();
+    leafletData.getMap('geoLocationMap').then(
+        map => {
+          console.log(pontos);
+          console.log(fronteira);
+          // pontos.addTo(map);
+          // map.fitBounds(pontos.getBounds())
+          map.fitBounds([
+              [40.712, -74.227],
+              [40.774, -74.125]
+          ]);
+        }
+    )
+      
 
   }
 }
