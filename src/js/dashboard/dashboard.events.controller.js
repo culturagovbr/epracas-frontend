cf9e24d6ff89d7760effc964ceb42dfeef3adee0
André  "decko" de Brito <decko@birosca.mobi>
2017-03-07 16:49:23 -0300

    WIP: Implementa o calendário dos gestores.

--- src/js/dashboard/dashboard.events.controller.js
+++ src/js/dashboard/dashboard.events.controller.js
@@ -1,7 +1,36 @@
 class DashboardEventsCtrl {
-  constructor($log) {
+  constructor($log, Atividade) {
     "ngInject";
+
+    this.config = {
+      calendarView: 'month',
+      viewDate: new Date(),
+    }
+
+    this.events = [];
+
+    Atividade.list()
+      // .then(events => events.map(this.returnEvent(a)))
+      .then(apiReturn => apiReturn.map(this.returnEvent))
+      .then(mappedEvents => {
+        //debugger;
+
+
+        // mappedEvents.forEach(this.events.push(event));
+        mappedEvents.forEach(event => this.events.push(event));
+      }
+      )
+      .catch($log.log('Erro na transformação de eventos'))
   }
-}
+
+  returnEvent(event) {
+    let newEvent = {
+        title: event.titulo,
+        startsAt: new Date(event.ocorrencia.start),
+        endsAt: new Date(event.ocorrencia.repeat_until)
+      }
+      return newEvent;
+    }
+  }
 
 export default DashboardEventsCtrl;
