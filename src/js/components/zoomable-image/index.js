aa56b566ff20d7cff48c9ca6e6eac91129a7bce8
André  "decko" de Brito <decko@birosca.mobi>
2017-03-14 09:26:37 -0300

    Adiciona o suporte ao Processo de Vinculação.

--- src/js/components/zoomable-image/index.js
+++ src/js/components/zoomable-image/index.js
@@ -1,9 +1,23 @@
 import Magnifier from 'magnifier'
 
 class MagnifierController {
-  constructor() {
+  constructor($scope, $document) {
     "ngInject";
 
+
+    // $scope.$watch(() => {
+    //   return document.getElementById('epracas-magnifier');
+    // }, () => {
+    //   let magnifier = new Magnifier();
+
+    //   magnifier.attach({
+    //     thumb: "#epracas-magnifier",
+    //     large: this.src,
+    //     zoom: 3,
+    //     zoomable: true,
+    //     mode: 'inside'
+    //   })
+    // });
     this.$postLink = () => {
       let magnifier = new Magnifier();
 
@@ -23,7 +37,7 @@ const ZoomableImage = {
   template: `
     <div>
       <div class="magnifier-thumb-wrapper">
-        <img id="epracas-magnifier" src="{{ $ctrl.src }}" />
+        <img flex="100" height="auto" id="epracas-magnifier" ng-src="{{ $ctrl.src }}" />
       </div>
     </div>
   `,
--- src/js/dashboard/vinculacao/dashboard.vinculo.controller.js
+++ src/js/dashboard/vinculacao/dashboard.vinculo.controller.js
@@ -1,13 +1,17 @@
 class DashboardVinculoCtrl {
-  constructor($scope, $http, $mdDialog, $document, $log, AppConstants) {
+  constructor($scope, $http, $mdDialog, $document, $log, Toast, Praca, AppConstants) {
     "ngInject";
 
     angular.extend(this, {
+      _$http: $http,
       _$mdDialog: $mdDialog,
       _$document: $document,
+      _Toast: Toast,
+      _Praca: Praca,
+      _AppConstants: AppConstants,
     })
 
-    this._$scope = $scope;
+    // this._$scope = $scope;
 
     this.pedidos = "";
     $http({
@@ -24,7 +28,24 @@ class DashboardVinculoCtrl {
     );
   }
 
-  processoVinculacao(pedido) {
+  showUserInfo(user) {
+    this._$mdDialog.show({})
+  }
+
+  showPracaInfo(praca) {
+    this._Praca.get(praca.id_pub)
+      .then(result => {
+        this._$mdDialog.show({
+          controller: 'PracaInfoCtrl',
+          controllerAs: '$ctrl',
+          templateUrl: 'praca/pracainfo-dialog.tmpl.html',
+          parent: angular.element(this._$document.body),
+          locals: { praca: result }
+        })
+      })
+  }
+
+  startProcessoVinculacao(pedido) {
     this._$mdDialog.show({
       controller: "DashboardVinculoDialogCtrl",
       controllerAs: "$ctrl",
@@ -32,6 +53,31 @@ class DashboardVinculoCtrl {
       parent: angular.element(this._$document.body),
       locals: { pedido: pedido },
     });
+  }
+
+  deleteProcessoVinculacao(pedido) {
+    this._$mdDialog.show(
+        this._$mdDialog.confirm()
+        .title("Excluir Pedido de Vinculação")
+        .textContent("Ao excluir um pedido de vinculação de um usuário à uma praça, você automaticamente excluir também os arquivos anexos a este pedido. Deseja continuar?")
+        .ariaLabel("Excluir Pedido de Vinculação")
+        .ok("Sim, excluir este pedido")
+        .cancel("Não, mantenha o pedido")
+        )
+        .then(
+            () => this._$http({
+              url: `${this._AppConstants.vinculoEndPoint}${pedido.id_pub}/`,
+              method: "DELETE",
+            })
+            .then( () => {
+              const index = this.pedidos.indexOf(pedido);
+              if (index > -1) {
+                this.pedidos.splice(index, 1);
+              };
+              this._Toast.showSuccessToast("Pedido excluido com sucesso");
+            })
+            .catch(err => this._Toast.showRejectedToast(`Problema ao excluir pedido ${err.status} - ${err.data}`))
+          )
   }
 
 }
