7434341a1b27ef911f3da17f95ea38463b221e76
Ruy Ferreira <ruyjfs@gmail.com>
2017-03-16 17:42:27 -0300

    - Ajuste na telha detalhar praca, tinha feito uma alteracao no arquivo errado.

--- src/js/praca/praca-detail.controller.js
+++ src/js/praca/praca-detail.controller.js
@@ -101,7 +101,7 @@ class PracaDetailCtrl {
 
                 let intPosition = $window.scrollY;
                 arrObjScrollContainers.each((intKey, objValue) => {
-                    if (intPosition >= objValue.intPositionStart && intPosition <= objValue.intPositionEnd && intKey != $scope.tabIntSelected) {
+                    if (intPosition >= objValue.intPositionStart && intKey != $scope.tabIntSelected) {
                         $scope.tabIntSelected = intKey;
                         $scope.$apply();
                         // console.info('Ativou Comeco:' + objValue.intPositionStart + ' Final ' + objValue.intPositionEnd );
