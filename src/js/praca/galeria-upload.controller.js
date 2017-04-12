75be165a44762df25b5862b72e2e1298e34f9b1b
André "decko" de Brito <brito.afa@gmail.com>
2017-04-12 14:27:18 -0300

    Insere Mock para exibir Parceiros e altera o comportamento de blocos na pagina de detalhes de uma praça.

--- src/js/praca/galeria-upload.controller.js
+++ src/js/praca/galeria-upload.controller.js
@@ -17,6 +17,21 @@ class UploadImgCtrl {
 
   uploadImg(imagem) {
 		if (imagem && imagem.length) {
+      this._$mdDialog.show({
+        clickOutsideToClose: false,
+        template: `
+          <div layout="row" layout-padding layout-align="center center">
+            <div flex=30>
+              <md-progress-circular md-mode="indeterminate"></md-progress-circular>
+            </div>
+            <div flex=70>
+              <p>Salvando arquivo {{i}} de {{$ctrl.imagem_count}}.
+              Por favor, aguarde.</p>
+            </div>
+          </div>
+          `,
+        locals: {i: this.i, imagem_count: imagem.length}
+      })
 			for (let i=0; i < imagem.length; i++) {
 				this._Upload.upload({
 					url: `${this._AppConstants.api}/pracas/${this.praca.id_pub}/imagens/`,
@@ -27,6 +42,7 @@ class UploadImgCtrl {
 							descricao: imagem[i].descricao,
 						}
 					})
+        this._$mdDialog.hide()
 				}
 			}
 		}
