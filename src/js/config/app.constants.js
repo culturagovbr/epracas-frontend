52f00a81b9c86c34c85eaf1f19d40f73123d2027
André "decko" de Brito <brito.afa@gmail.com>
2017-05-01 18:23:51 -0300

    WIP: Adiciona o suporte ao endpoint de parceiros

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -1,18 +1,19 @@
-const AppConstants = {};
+const AppConstants = {}
 
-AppConstants.appName = "e-Praças";
+AppConstants.appName = "e-Praças"
 
-AppConstants.jwtKey = "id_token";
-AppConstants.idcultura_url = "/* @echo IDCULTURA_URL */";
-AppConstants.issuerUri = AppConstants.idcultura_url.replace("https", "http");
-AppConstants.loginUrl = `${AppConstants.idcultura_url}/openid/connect/authorize`;
-AppConstants.userinfoUrl = `${AppConstants.idcultura_url}/api/v1/person`;
-AppConstants.clientId = "/* @echo IDCULTURA_CLIENTID */";
-AppConstants.logoutUrl = `${AppConstants.idcultura_url}/openid/connect/session/end?post_logout_redirect_uri=`;
+AppConstants.jwtKey = "id_token"
+AppConstants.idcultura_url = "/* @echo IDCULTURA_URL */"
+AppConstants.issuerUri = AppConstants.idcultura_url.replace("https", "http")
+AppConstants.loginUrl = `${AppConstants.idcultura_url}/openid/connect/authorize`
+AppConstants.userinfoUrl = `${AppConstants.idcultura_url}/api/v1/person`
+AppConstants.clientId = "/* @echo IDCULTURA_CLIENTID */"
+AppConstants.logoutUrl = `${AppConstants.idcultura_url}/openid/connect/session/end?post_logout_redirect_uri=`
 
-AppConstants.api = "/* @echo EPRACAS_API_URL */";
-AppConstants.apiUserInfo = `${AppConstants.api}/user/`;
-AppConstants.agendaApi = `${AppConstants.api}/atividades/`;
-AppConstants.vinculoEndPoint = `${AppConstants.api}/processo/`;
+AppConstants.api = "/* @echo EPRACAS_API_URL */"
+AppConstants.apiUserInfo = `${AppConstants.api}/user/`
+AppConstants.agendaApi = `${AppConstants.api}/atividades/`
+AppConstants.vinculoEndPoint = `${AppConstants.api}/processo/`
+AppConstants.parceirosEndPoint = `${AppConstants.api}/parceiros/`
 
-export default AppConstants;
+export default AppConstants
