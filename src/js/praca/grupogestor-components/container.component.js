32ad2320e3761e0f6a1b3f8c2b862968a601bde7
André "decko" de Brito <brito.afa@gmail.com>
2017-05-19 09:53:05 -0300

    Mostra o botão de adicionar no container do Grupo Gestor.

--- src/js/praca/grupogestor-components/container.component.js
+++ src/js/praca/grupogestor-components/container.component.js
@@ -1,13 +1,23 @@
+import GestorDialog from "./dialog.component"
+
+class GrupoGestorController {
+  constructor($mdDialog) {
+    "ngInject"
+
+    angular.extend(this, {
+      $mdDialog,
+    })
+  }
+
+  showGestorDialog() {
+    this.$mdDialog.show(GestorDialog)
+  }
+}
+
 const GrupoGestorContainer = {
+  controller: GrupoGestorController,
   template: `
-    <md-content id="container-grupogestor" layout-gt-xs="row">
-      <md-fab-speed-dial class="md-warm md-fab-top-right">
-        <md-fab-trigger>
-          <md-button class="md-fab">
-            <md-icon>add</md-icon>
-          </md-button>
-        </md-fab-trigger>
-      </md-fab-speed>
+    <div id="container-grupogestor" layout-padding layout-gt-xs="row">
       <div flex layout-padding class="info">
         <div class="layout-row title">
           <md-icon>group</md-icon>
@@ -16,8 +26,15 @@ const GrupoGestorContainer = {
         <div ng-show="!$ctrl.grupo_gestor">
           <p>Os dados sobre o Grupo Gestor ainda não foram inseridos nesta Praça.</p>
         </div>
+      <md-fab-speed-dial class="md-fab-top-right" ng-click="$ctrl.showGestorDialog()">
+        <md-fab-trigger>
+          <md-button class="md-fab">
+            <md-icon>add</md-icon>
+          </md-button>
+        </md-fab-trigger>
+      </md-fab-speed>
       </div>
-    </md-content>
+    </div>
     `,
   bindings: {
     grupo_gestor: "<",
