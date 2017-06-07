63423590de8eb9ba7dde3fec343691d33666d3cf
Ruy Ferreira <ruyjfs@gmail.com>
2017-06-07 16:44:51 -0300

    - Ajustes no componente de galeria da pracas. - Implementacao do icone para editar e excluir uma foto da galeria.

--- src/js/praca/galeria-components/praca-galeria-content.component.js
+++ src/js/praca/galeria-components/praca-galeria-content.component.js
@@ -1,22 +1,26 @@
 class PracaGaleriaContentController {
-  constructor($attrs, Praca, $scope, $stateParams, $document, $mdMedia) {
+  constructor($attrs, Praca, $scope, $stateParams, $document, $mdMedia, $mdMenu) {
     "ngInject";
       $scope.imagens = [];
       Praca.getImages($stateParams.pk).then(function(arrResult) {
           $scope.imagens = arrResult;
       });
 
+      this.$mdMenu = $mdMenu;
+      // this.$event = $event;
+
       $document.ready(() =>{
         $('.materialboxed').materialbox();
       });
       this.paginator = {};
       this.paginator.page = 1;
+      this.paginator.perpage = 9;
       if ($mdMedia('sm')) {
           this.paginator.perpage = 6;
       } else if ($mdMedia('lg')) {
-          this.paginator.perpage = 12;
+          this.paginator.perpage = 10;
       } else if ($mdMedia('gt-lg')) {
-          this.paginator.perpage = 18;
+          this.paginator.perpage = 15;
       }
       $scope.paginatorData = [];
       this.paginator.intRendered = 0;
@@ -28,8 +32,14 @@ class PracaGaleriaContentController {
           }
       );
       this.scope = $scope;
-  }
 
+
+  }
+    openMenu($mdMenu, ev) {
+        originatorEv = ev;
+        $mdMenu.open(ev);
+        console.info('aa');
+    };
     /**
      * Constroi o objecto da grid em mozaico com as imagens da praca ficando no formato que o componente do Material Angular espera.
      * @param {Array} arrValue - Array com as imagens das pracas.
@@ -72,7 +82,7 @@ const PracaGaleriaContent = {
   controllerAs: "$ctrl",
   template: `
     <md-grid-list
-      md-cols="1" md-cols-sm="2" md-cols-md="3" md-cols-gt-md="6"
+      md-cols="1" md-cols-sm="2" md-cols-md="3" md-cols-gt-md="5"
       md-row-height-gt-md="1:1" md-row-height="4:3"
       md-gutter="8px" md-gutter-gt-sm="4px"
       class="content-gallery">
@@ -84,7 +94,32 @@ const PracaGaleriaContent = {
                     md-colspan-xs="1"
                     class="animated fadeIn">
         <img width="135%" class="materialboxed animated fadeIn" data-caption="{{objPhoto.title}}" ng-src="{{objPhoto.url}}">
-        <md-grid-tile-footer><h3>{{objPhoto.title}}</h3></md-grid-tile-footer>
+        <md-grid-tile-footer>
+        <h3 class="left">{{objPhoto.title}}
+        <md-menu style="
+    position: absolute;
+    right: 0px;
+    top: 5px;"
+    show-as-manager="true">
+          <md-button aria-label="Open phone interactions menu" class="md-icon-button" ng-click="$mdMenu.open()">
+            <i class="material-icons right">more_vert</i>
+          </md-button>
+          <md-menu-content width="4">
+            <md-menu-item>
+              <md-button ng-click="ctrl.redial($event)">
+                <i class="material-icons left">edit</i>
+                Editar
+              </md-button>
+            </md-menu-item>
+            <md-menu-item>
+              <md-button ng-click="ctrl.checkVoicemail()">
+                <i class="material-icons left">delete</i>
+                Excluir
+              </md-button>
+            </md-menu-item>
+          </md-menu-content>
+        </md-menu>
+        </h3></md-grid-tile-footer>
       </md-grid-tile>
     </md-grid-list>
     <div id="container-btmais" class="row md-padding">
