4bed45544efd6b08901b2d09392a71d0d0452398
André "decko" de Brito <brito.afa@gmail.com>
2017-06-08 05:35:49 -0300

    Implementa a recuperação de opções de alguns campos referentes a GrupoGestor e MembroGestor.

--- src/js/services/grupogestor.service.js
+++ src/js/services/grupogestor.service.js
@@ -13,6 +13,9 @@ class GrupoGestor {
 
   save_grupogestor(praca, grupogestor) {
     grupogestor.data_instituicao = moment(grupogestor.data_instituicao).format("YYYY-MM-DD")
+    if (grupogestor.data_finalizacao) {
+      grupogestor.data_finalizacao = moment(grupogestor.data_finalizacao).format("YYYY-MM-DD")
+    }
     if (grupogestor.id_pub) {
       delete grupogestor.documento_constituicao
       return this.$http({
@@ -35,16 +38,29 @@ class GrupoGestor {
     })
   }
 
+  options_grupogestor(praca) {
+    return this.$http({
+      url: `${this.AppConstants.pracaEndPoint}${praca.id_pub}/grupogestor/`,
+      method: "OPTIONS",
+    })
+    .then(response => response.data)
+    .then(data => data.actions.POST)
+  }
+
   save_membrogestor(praca, grupogestor, membrogestor) {
+    membrogestor.data_posse = moment(membrogestor.data_posse).format("YYYY-MM-DD")
+    if (membrogestor.data_desligamento) {
+      membrogestor.data_desligamento = moment(membrogestor.data_desligamento).format("YYYY-MM-DD")
+    }
     if (membrogestor.id_pub) {
       return this.$http({
-        url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor.id_pub}/membrogestor/${membrogestor.id_pub}/`,
+        url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor}/membrogestor/${membrogestor.id_pub}/`,
         method: "PATCH",
         data: membrogestor,
       })
     }
     return this.$http({
-      url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor.id_pub}/membrogestor/${membrogestor.id_pub}/`,
+      url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor}/membrogestor/`,
       method: "POST",
       data: membrogestor,
     })
@@ -52,9 +68,18 @@ class GrupoGestor {
 
   delete_membrogestor(praca, grupogestor, membrogestor) {
     return this.$http({
-      url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor.id_pub}/membrogestor/${membrogestor.id_pub}/`,
+      url: `${this.AppConstants.pracaEndPoint}${praca}/grupogestor/${grupogestor}/membrogestor/${membrogestor}/`,
       method: "DELETE",
     })
+  }
+
+  options_membrogestor(praca) {
+    return this.$http({
+      url: `${this.AppConstants.pracaEndPoint}${praca.id_pub}/grupogestor/${praca.grupo_gestor.id_pub}/membrogestor/`,
+      method: "OPTIONS",
+    })
+    .then(response => response.data)
+    .then(data => data.actions.POST)
   }
 }
 
