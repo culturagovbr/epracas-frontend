45a0271be086d69cee95ba6a982856b4b43531d4
André "decko" de Brito <brito.afa@gmail.com>
2017-06-08 05:44:23 -0300

    Implementa a recuperação de algumas opções através de service, e altera os parametros recebidos pela view.

--- src/js/praca/grupogestor-components/membrogestor.dialog.ctrl.js
+++ src/js/praca/grupogestor-components/membrogestor.dialog.ctrl.js
@@ -8,30 +8,39 @@ export default class MembroGestorDialogController {
       $log,
       Toast,
       GrupoGestor,
-      praca
+      praca,
     })
 
-    this.listaOrigem = [
-      { value: 1, display_name: "Poder Publico" },
-      { value: 2, display_name: "Sociedade Civil" },
-      { value: 3, display_name: "Moradores do Entorno" },
-    ]
+    this.GrupoGestor.options_grupogestor(praca)
+      .then((data) => {
+        this.listaConstituicao = data.tipo_documento.choices
+      })
 
-    this.listaConstituicao = [
-      { value: 1, display_name: "Decreto" },
-      { value: 2, display_name: "Portaria" },
-      { value: 3, display_name: "Lei" },
-      { value: 4, display_name: "Não formalizado" },
-    ]
+    this.GrupoGestor.options_membrogestor(praca)
+      .then((data) => {
+        this.listaOrigem = data.origem.choices
+      })
+
+    // this.listaOrigem = [
+    //   { value: "pp", display_name: "Poder Publico" },
+    //   { value: "sc", display_name: "Sociedade Civil" },
+    //   { value: "me", display_name: "Moradores do Entorno" },
+    // ]
+
+    // this.listaConstituicao = [
+    //   { value: "d", display_name: "Decreto" },
+    //   { value: "p", display_name: "Portaria" },
+    //   { value: "l", display_name: "Lei" },
+    //   { value: "n", display_name: "Não formalizado" },
+    // ]
   }
 
   cancel() { this.$mdDialog.cancel() }
 
   save(praca, membro) {
     this.isSaving = true
-    const gg = praca.grupo_gestor[0]
 
-    this.GrupoGestor.save_membrogestor(praca, gg, membro)
+    this.GrupoGestor.save_membrogestor(praca.id_pub, praca.grupo_gestor.id_pub, membro)
       .then(
         (response) => {
           this.$mdDialog.hide()
@@ -40,19 +49,19 @@ export default class MembroGestorDialogController {
       )
       .catch(
         (err) => {
-          this.$log.error(`Erro ao salvar membro gestor ${err.status}, {err.data}`)
+          this.$log.error(`Erro ao salvar membro gestor ${angular.toJson(err.status)}, ${angular.toJson(err.data)}`)
           this.Toast.showRejectedToast("Erro ao adicionar gestor")
         }
       )
   }
 
   showFinalizaGrupoGestorDialog(praca) {
-    const gg = praca.grupo_gestor
+    const grupogestor = praca.grupo_gestor
     this.$mdDialog.show({
       controller: "GrupoGestorDialogController",
       controllerAs: "$ctrl",
       templateUrl: "praca/grupogestor-components/finaliza-grupogestor.dialog.tmpl.html",
-      locals: { praca, gg },
+      locals: { praca, grupogestor },
       fullscreen: true,
     })
   }
