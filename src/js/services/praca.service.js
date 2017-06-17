5e39a4b13a039a272f2384f0c212ee9568a37af9
André "decko" de Brito <brito.afa@gmail.com>
2017-06-17 03:02:15 -0300

    Passa a utilizar o novo service de captura de erros.

--- src/js/services/praca.service.js
+++ src/js/services/praca.service.js
@@ -1,125 +1,205 @@
 export default class Praca {
-  constructor(AppConstants, $http, $q) {
-    "ngInject";
+  constructor($http, $q, ErrorCatcher, AppConstants) {
+    "ngInject"
 
-    this._AppConstants = AppConstants;
-    this._PracaEndPoint = `${AppConstants.api}/pracas/`;
-
-    this._$http = $http;
-    this._$q = $q;
+    angular.extend(this, {
+      $http,
+      $q,
+      ErrorCatcher,
+      AppConstants,
+      PracaEndPoint: `${AppConstants.api}/pracas/`,
+    })
   }
 
   // Cria uma nova Praça e retorna a um JSON com os dados
   new(data) {
-    return this._$http({
-      url: this._PracaEndPoint,
-      method: 'POST',
-      data: data
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$http({
+      url: this.PracaEndPoint,
+      method: "POST",
+      data: data,
     })
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Exclui uma Praça
   delete(pk) {
-    return this._$http({
-      url: `${this._PracaEndPoint}${pk}/`,
-      method: 'DELETE'
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$http({
+      url: `${this.PracaEndPoint}${pk}/`,
+      method: "DELETE",
     })
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Recupera o registro de uma Praça
   get(pk) {
-    return this._$q((resolve, reject) => {
-      if(!pk.replace(" ", "")) {
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$q((resolve, reject) => {
+      if (!pk.replace(" ", "")) {
         reject("O id_pub está vazio!")
       } else {
         resolve()
       }
     })
-    .then(() => this._$http({
-      url: `${this._PracaEndPoint}${pk}/`,
-      method: 'GET'
+    .then(() => this.$http({
+      url: `${this.PracaEndPoint}${pk}/`,
+      method: "GET",
     }))
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Recupera as imagens de uma praca
   getImages(pk) {
-    return this._$q((resolve, reject) => {
-      if(!pk.replace(" ", "")) {
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$q((resolve, reject) => {
+      if (!pk.replace(" ", "")) {
         reject("O id_pub está vazio!")
       } else {
         resolve()
       }
     })
-    .then(() => this._$http({
-      url: `${this._PracaEndPoint}${pk}/imagens/`,
-      method: 'GET'
+    .then(() => this.$http({
+      url: `${this.PracaEndPoint}${pk}/imagens/`,
+      method: "GET",
     }))
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Recupera as imagens de uma praca
   deleteImg(pkPraca, pkImg) {
-    return this._$q((resolve, reject) => {
-      if(!pkPraca.replace(" ", "")) {
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$q((resolve, reject) => {
+      if (!pkPraca.replace(" ", "")) {
         reject("O id_pub está vazio!")
       } else {
         resolve()
       }
     })
-    .then(() => this._$http({
-      url: `${this._PracaEndPoint}${pkPraca}/imagens/${pkImg}/`,
-      method: 'DELETE'
+    .then(() => this.$http({
+      url: `${this.PracaEndPoint}${pkPraca}/imagens/${pkImg}/`,
+      method: "DELETE",
     }))
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Lista todas as Praças
   list() {
-    return this._$http({
-      url: this._PracaEndPoint,
-      method: "GET"
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$http({
+      url: this.PracaEndPoint,
+      method: "GET",
     })
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      },
+    )
   }
 
   // Procura por uma Praça utilizando o parametro query
   search(query) {
-    return this._$http({
-      url: this._PracaEndPoint,
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$http({
+      url: this.PracaEndPoint,
       method: "GET",
-      params: { search: query }
+      params: { search: query },
     })
     .then(res => res.data)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 
   // Salva os dados de uma Praça
   save(praca, data) {
-    return this._$http({
-      url: `${this._PracaEndPoint}${praca}/`,
-      method: 'PATCH',
-      data: data
+    const caller = this.ErrorCatcher.callerName()
+
+    return this.$http({
+      url: `${this.PracaEndPoint}${praca}/`,
+      method: "PATCH",
+      data: data,
     })
-    .then(res => res.data)
+      .then(res => res.data)
+      .catch(
+        (err) => {
+          this.ErrorCatcher.error(caller, err)
+          return this.$q.reject()
+        }
+      )
   }
 
   // Retorna as opções disponiveis para alguns campos
   options(praca) {
+    const caller = this.ErrorCatcher.callerName()
+
     if (praca) {
-      return this._$http({
-        url: `${this._PracaEndPoint}${praca.id_pub}/`,
+      return this.$http({
+        url: `${this.PracaEndPoint}${praca.id_pub}/`,
         method: "OPTIONS",
       })
       .then(response => response.data)
       .then(data => data.actions.PUT)
+      .catch(
+        (err) => {
+          this.ErrorCatcher.error(caller, err)
+          return this.$q.reject()
+        }
+      )
     }
-    return this._$http({
-      url: this._PracaEndPoint,
+    return this.$http({
+      url: this.PracaEndPoint,
       method: "OPTIONS",
     })
     .then(response => response.data)
     .then(data => data.actions.POST)
+    .catch(
+      (err) => {
+        this.ErrorCatcher.error(caller, err)
+        return this.$q.reject()
+      }
+    )
   }
 }
