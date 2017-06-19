c3302220fcd707c23ed9374317a564703456f930
André "decko" de Brito <brito.afa@gmail.com>
2017-06-19 11:07:22 -0300

    Altera alguns campos para Membro da Unidade Gestora Local.

--- src/js/praca/unidadegestora-components/container.component.js
+++ src/js/praca/unidadegestora-components/container.component.js
@@ -9,12 +9,11 @@ class UnidadeGestoraController {
   }
 
   showUGLDialog(praca) {
-    const grupogestor = praca.grupo_gestor
     this.$mdDialog.show({
       controller: "MembroUglDialogController",
       controllerAs: "$ctrl",
       templateUrl: "praca/unidadegestora-components/membrougl.dialog.tmpl.html",
-      locals: { praca, grupogestor },
+      locals: { praca },
       fullscreen: true,
     })
   }
@@ -36,9 +35,8 @@ const UnidadeGestoraContainer = {
 
         <div layout-wrap layout-margin layout="row">
           <md-card  ng-repeat="membro in $ctrl.praca.unidade_gestora" layout-padding flex="18">
-            <div class="epr-avatar" style="background-image: url('{{ membro.imagem}}')"></div>
             <span class="epr-name">{{ membro.nome }}</span>
-            <span class="epr-subname">{{ membro.origem_descricao }}</span>
+            <span class="epr-subname">{{ membro.tipo }}</span>
           </md-card>
         </div>
 
