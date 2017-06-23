ec22c603b0ed13d85743e1ef6251a8d2707671d1
Ruy Ferreira <ruyjfs@gmail.com>
2017-06-23 19:28:05 -0300

    Tela de agenda trocando de ano e mes.

--- src/js/dashboard/dashboard.events.controller.js
+++ src/js/dashboard/dashboard.events.controller.js
@@ -24,41 +24,23 @@ class DashboardEventsCtrl {
         });
     }
 
-    console.info(this.arrMonth)
-
     this.config = {
       calendarView: 'month',
       viewDate: new Date(),
     };
 
     this.events = [];
-
-    Atividade.list(null, intMonth, intYear)
-      // .then(events => events.map(this.returnEvent(a)))
-      .then(apiReturn => apiReturn.map(this.returnEvent))
-      .then(mappedEvents => {
-        //debugger;
-
-        // mappedEvents.forEach(this.events.push(event));
-        mappedEvents.forEach(event => this.events.push(event));
-      }
-      )
-      .catch($log.log('Erro na transformação de eventos'));
-
-
-
-
-
-      this.changeDate = () => {
+    this.loadEvents = () => {
           this.events = [];
           Atividade.list(null, this.objForm.intMonth, this.objForm.intYear)
-              .then(apiReturn => apiReturn.map(this.returnEvent))
-              .then(mappedEvents => {
-                      mappedEvents.forEach(event => this.events.push(event));
-                  }
-              )
-              .catch($log.log('Erro na transformação de eventos'));
-      };
+          // .then(events => events.map(this.returnEvent(a)))
+          .then(apiReturn => apiReturn.map(this.returnEvent))
+          .then(mappedEvents => {
+              mappedEvents.forEach(event => this.events.push(event));
+          }).catch($log.log('Erro na transformação de eventos'));
+      }
+
+      this.loadEvents();
   }
 
   returnEvent(event) {
