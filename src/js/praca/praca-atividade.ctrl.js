29e28284748b617aee236add1c433ebda6c5c558
Ruy Ferreira <ruyjfs@gmail.com>
2017-07-18 16:14:21 -0300

    Ajustes na tela de atividades e de galeria.

--- src/js/praca/praca-atividade.ctrl.js
+++ src/js/praca/praca-atividade.ctrl.js
@@ -5,66 +5,19 @@ class PracaAtividadeCtrl {
     "ngInject";
     angular.extend(this, {$scope});
 
-      // console.info(objData)
-
       Atividade.options().then(
           (data) => {
-              objData.tipos = data.tipo.choices;
-              objData.espacos = data.espaco.child.choices;
-              objData.faixa_etarias = data.faixa_etaria.child.choices;
-              objData.territorios = data.territorio.choices;
-              objData.publicos = data.publico.choices;
+              objData.tipos = data.tipo.choices.filter((objValue) => {return (objData.tipo == objValue.value)});
+              objData.espacos = data.espaco.child.choices.filter((objValue) => {return (objData.espaco.indexOf(objValue.value) >= 0)});
+              objData.faixa_etarias = data.faixa_etaria.child.choices.filter((objValue) => {return (objData.faixa_etaria.indexOf(objValue.value) >= 0)});
+              objData.territorios = data.territorio.choices.filter((objValue) => {return (objData.territorio == objValue.value)});
+              objData.publicos = data.publico.choices.filter((objValue) => {return (objData.publico == objValue.value)});
           }
       );
-
-      console.info(objData)
-
-
-      // console.info(objData)
-      // objData.options()
-      //     .then((obj) => {
-      //         console.info(obj)
-      //     });
-
 
     objData.ocorrencia.repeat_until = moment(objData.ocorrencia.repeat_until).format("DD/MM/YYYY");
     objData.ocorrencia.start = moment(objData.ocorrencia.start).format("DD/MM/YYYY");
     this.objData = objData;
-
-    // let arrImgHeight = [];
-    // arrImgHeight[1] = '150px';
-    // arrImgHeight[2] = '200px';
-    // arrImgHeight[3] = '250px';
-    // this.intImgHeght = () => {
-    //   return arrImgHeight[Math.floor((Math.random() * 3) + 1)];
-    // };
-
-      // $scope.imagens = imagens;
-      //
-      // $document.ready(() =>{
-      //     $('.materialboxed').materialbox();
-      // });
-      //
-      // this.paginator = {};
-      // this.paginator.page = 1;
-      // this.paginator.perpage = 2;
-      // $scope.paginatorData = [];
-      // this.paginator.intRendered = 0;
-      //
-      // // this.imagens = imagens;
-      // this.arrImg = this.buildGridModel(imagens);
-      // this.paginatorRender($scope);
-      // this.scope = $scope;
-
-      // $scope.$watch('paginatorData',
-      //     () => {
-              // $('.materialboxed').materialbox();
-              // console.info('aa1');
-              // console.info('aa2');
-              // this.currentUser = newUser
-              // this.userMenu = this.buildMenu(this.currentUser)
-      //     }
-      // );
   }
 }
 
