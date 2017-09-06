3dd9b5fcfbe1be3f05b9af12375daea0f6ec5283
Ruy <ruyjfs@gmail.com>
2017-09-06 17:09:49 -0300

    - Dashboard - Eventos -- Correcao ao trocar a data do evento trocar tbm do calendario e nao somente recarregar os eventos.

--- src/js/dashboard/dashboard.events.controller.js
+++ src/js/dashboard/dashboard.events.controller.js
@@ -1,56 +1,62 @@
 import moment from "moment"
 
 class DashboardEventsCtrl {
-  constructor($log, Atividade, $stateParams) {
-    "ngInject";
+    constructor($log, Atividade, $stateParams) {
+        "ngInject";
 
-    let intYear = (typeof $stateParams.year === 'undefined')? moment().format('YYYY') : $stateParams.year,
-        intMonth = (typeof $stateParams.month === 'undefined')? parseInt(moment().format('MM')) : $stateParams.month;
+        let intYear = (typeof $stateParams.year === 'undefined') ? moment().format('YYYY') : $stateParams.year,
+            intMonth = (typeof $stateParams.month === 'undefined') ? parseInt(moment().format('MM')) : $stateParams.month;
 
-    this.objForm = {
-        intYear: intYear,
-        intMonth: intMonth
-    };
+        this.objForm = {
+            intYear: intYear,
+            intMonth: intMonth
+        };
 
-    // Montando os meses para o formulario.
-    this.arrMonth = moment.months().map((data, intIndex) => {
-        return {id : intIndex+1, name : data};
-    });
-    this.arrYear = [];
-    for (let i = 2017; i < parseInt(moment().format('YYYY')) + 2; i++) {
-        this.arrYear.push({
-            id : i,
-            name : i
+        // Montando os meses para o formulario.
+        this.arrMonth = moment.months().map((data, intIndex) => {
+            return {id: intIndex + 1, name: data};
         });
+        this.arrYear = [];
+        for (let i = 2017; i < parseInt(moment().format('YYYY')) + 2; i++) {
+            this.arrYear.push({
+                id: i,
+                name: i
+            });
+        }
+
+        let dateCalendar = new Date();
+        this.config = {
+            calendarView: 'month',
+            viewDate: dateCalendar,
+        };
+
+        this.events = [];
+        this.loadEvents = () => {
+            dateCalendar = new Date(this.objForm.intMonth + '-' + '01' + '-' + this.objForm.intYear);
+            this.config = {
+                calendarView: 'month',
+                viewDate: dateCalendar,
+            };
+            this.events = [];
+            Atividade.list(null, this.objForm.intMonth, this.objForm.intYear)
+            // .then(events => events.map(this.returnEvent(a)))
+                .then(apiReturn => apiReturn.map(this.returnEvent))
+                .then(mappedEvents => {
+                    mappedEvents.forEach(event => this.events.push(event));
+                }).catch($log.log('Erro na transformação de eventos'));
+        };
+
+        this.loadEvents();
     }
 
-    this.config = {
-      calendarView: 'month',
-      viewDate: new Date(),
-    };
-
-    this.events = [];
-    this.loadEvents = () => {
-          this.events = [];
-          Atividade.list(null, this.objForm.intMonth, this.objForm.intYear)
-          // .then(events => events.map(this.returnEvent(a)))
-          .then(apiReturn => apiReturn.map(this.returnEvent))
-          .then(mappedEvents => {
-              mappedEvents.forEach(event => this.events.push(event));
-          }).catch($log.log('Erro na transformação de eventos'));
-      };
-
-      this.loadEvents();
-  }
-
-  returnEvent(event) {
-    let newEvent = {
-        title: event.titulo,
-        startsAt: new Date(event.ocorrencia.start),
-        endsAt: new Date(event.ocorrencia.repeat_until)
-      };
-      return newEvent;
+    returnEvent(event) {
+        let newEvent = {
+            title: event.titulo,
+            startsAt: new Date(event.ocorrencia.start),
+            endsAt: new Date(event.ocorrencia.repeat_until)
+        };
+        return newEvent;
     }
-  }
+}
 
 export default DashboardEventsCtrl;
