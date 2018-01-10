52cdee2fb045034c686ec9579caf5a09ecb3e10d
leonardo <volthier@gmail.com>
2018-01-10 15:41:13 -0200

    [UPDATE] Filtro de Estado finalizado e operacional no front-end

--- src/js/dashboard/dashboard.events.controller.js
+++ src/js/dashboard/dashboard.events.controller.js
@@ -7,9 +7,13 @@ class DashboardEventsCtrl {
 
         this._$state = $state;
 
+        //montando seletor de Estados(uf)
+        this.arrUf = [];
+        this.arrUf.unshift({ value: -1, display_name: "Todos" });
+
         let intYear = (typeof $stateParams.year === 'undefined') ? moment().format('YYYY') : $stateParams.year,
             intMonth = (typeof $stateParams.month === 'undefined') ? parseInt(moment().format('MM')) : $stateParams.month,
-            uf = (typeof $stateParams.uf === 'undefined') ? null : $stateParams.uf;
+            uf = (typeof $stateParams.uf === 'undefined') ? this.arrUf.indexOf(0) : $stateParams.uf;
 
         this.objForm = {
             intYear: intYear,
@@ -19,8 +23,9 @@ class DashboardEventsCtrl {
 
         // Montando os meses para o formulario.
         this.arrMonth = moment.months().map((data, intIndex) => {
-            return {id: intIndex + 1, name: data};
+            return { id: intIndex + 1, name: data };
         });
+
         this.arrYear = [];
         for (let i = 2017; i < parseInt(moment().format('YYYY')) + 2; i++) {
             this.arrYear.push({
@@ -35,46 +40,70 @@ class DashboardEventsCtrl {
             viewDate: dateCalendar,
         };
 
-        this.arrUf = [];
         this.events = [];
         this.loadEvents = () => {
+
+            this.arrUf = this.arrUf.sort();
+
             dateCalendar = new Date(this.objForm.intYear + '-' + this.objForm.intMonth + '-' + '01 12:00:00'); // Firefox nao e tao flexivel quanto o chrome, por isso foi colocado as horas.
             this.config = {
                 calendarView: 'month',
                 viewDate: dateCalendar,
             };
-            this.arrUf = [];
+
+            //Resetando filtro evitando duplicidade de UF/estados
+            if (this.objForm.uf == -1) {
+                this.arrUf = [];
+                this.arrUf.unshift({ value: -1, display_name: "Todos" });
+            }
+
             this.events = [];
-            Atividade.list(null, this.objForm.intMonth, this.objForm.intYear)
+            Atividade.list(null, this.objForm.intMonth, this.objForm.intYear, this.objForm.uf)
                 .then(apiReturn => apiReturn.map(this.returnEvent))
                 .then(mappedEvents => {
+
                     mappedEvents.forEach(event => {
-                        this.events.push(event);
-                        if (this.arrUf.indexOf(event.uf.toLowerCase()) < 0) {
-                            this.arrUf.push(event.uf.toLowerCase());
+                        if (this.objForm.uf == -1) {
+                            this.events.push(event);
+                        } else if (this.objForm.uf != -1) {
+                            if (event.uf.toUpperCase() == this.objForm.uf.toUpperCase()) {
+                                this.events.push(event);
+                            }
                         }
-                           this.arrUf = this.arrUf.sort();
+
+                        if (this.objForm.uf == -1) {
+                            if (this.arrUf.indexOf(event.uf.toLowerCase()) < 0) {
+                                this.arrUf.push(event.uf.toLowerCase());
+                            }
+                        }
+                        this.arrUf = this.arrUf.sort();
                     });
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $('div.cal-month-day:not(.cal-day-today)').removeClass('cal-day-event');
                         $('small.cal-events-num:not(.ng-hide)').closest('div.cal-month-day:not(.cal-day-today)').addClass('cal-day-event');
                     }, 500);
-               
+
                     Praca.options().then((data) => {
-                        angular.forEach(data.uf.choices, (uf)=>{
-                            if(this.arrUf.indexOf(uf.value) in this.arrUf){
-                            this.arrUf.splice(this.arrUf.indexOf(uf.value),1, uf);
-                        }
+                        angular.forEach(data.uf.choices, (uf) => {
+                            var i = 0;
+
+                            if (this.arrUf.indexOf(uf.display_name) in this.arrUf) {
+                                i = 1;
+                            }
+                            if (i != 1) {
+                                if (this.arrUf.indexOf(uf.value) in this.arrUf) {
+                                    this.arrUf.splice(this.arrUf.indexOf(uf.value), 1, uf);
+                                }
+                            }
                         });
                     });
+
                     this.arrUf = this.arrUf.sort();
 
-                }
-            
-            ).catch($log.log('Erro na transformação de eventos'));
+                }).catch($log.log('Erro na transformação de eventos'));
         };
         this.navigateTo = (pk) => {
-            this._$state.go('app.atividade', {pk: pk});
+            this._$state.go('app.atividade', { pk: pk });
         };
         this.loadEvents();
     }
@@ -90,7 +119,6 @@ class DashboardEventsCtrl {
         };
         return newEvent;
     }
-
 }
 
 export default DashboardEventsCtrl;
