12af2cc7bc3f0cf0edbc736787a475e050fb23c8
Ruy <ruyjfs@gmail.com>
2018-01-11 14:16:07 -0200

    Documentacao - implementacao de alguns dados dos parceiros na tela de detalhacao da praca.

--- src/js/praca/parceiros-components/detail.component.js
+++ src/js/praca/parceiros-components/detail.component.js
@@ -3,7 +3,11 @@ const ParceirosDetailElement = {
     <md-card layout-padding>
       <div ng-show="$ctrl.parceiro.imagem" class="epr-avatar" style="background-image: url('{{ $ctrl.parceiro.imagem}}')"></div>
       <span class="epr-name">{{ $ctrl.parceiro.nome }}</span>
-      <span class="epr-subname"></span>
+      <span class="epr-subname">
+        <p><b>Nome: </b>{{$ctrl.parceiro.nome}}</p>
+        <p><b>Email: </b>{{$ctrl.parceiro.email}}</p>
+        <p><b>Atividade: </b>{{$ctrl.parceiro.ramo_atividade_name}}</p>
+      </span>
     </md-card>
     `,
   bindings: {
