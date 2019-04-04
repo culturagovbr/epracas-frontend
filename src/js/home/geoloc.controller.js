9802edeeeb4c4202c1df1cb4e2e08137676c872e
Pedro Philipe Alves de Oliveira <pedrophilipe.ti@gmail.com>
2019-04-03 22:05:05 -0300

    Fix #98

--- src/js/home/geoloc.controller.js
+++ src/js/home/geoloc.controller.js
@@ -12,15 +12,9 @@ class GeolocCtrl {
         },
         scrollWheelMouse: false,
         doubleClickZoom: false,
-        zoomControl: false,
-        dragging: false,
+        zoomControl: true,
       },
       markers: {},
-      center: {
-        lat: Number(40.712),
-        lng: Number(-74.125),
-        zoom: 1,
-      },
     }
 
     this.pracas_proximas = [];
@@ -28,43 +22,33 @@ class GeolocCtrl {
 
     GeoLocation.getCurrentPosition()
       .then(position => GeoLocation.getDistanceList(position))
-      .then(distances => distances.forEach((praca, index) => {
-        this.pracas_proximas.push(praca);
+        .then(distances => distances.forEach((praca, index, pracas) => {
+          this.pracas_proximas.push(praca);
 
-        let pracaId = praca.id_pub.split("-")[0];
-        this.geoLoc.markers[pracaId] = {
-          lat: parseFloat(praca.latlong.split(', ')[0]),
-          lng: parseFloat(praca.latlong.split(', ')[1]),
-          message: praca.nome,
-          draggable: false,
-          focus: false
-        }
-
-        const marker = L.marker(praca.latlong.split(',')[0], praca.latlong.split(',')[1]);
-        bounds.push(marker);
-      }
-      )
-      )
-      .catch(error => console.warn(error))
-
-
-    const pontos = new L.featureGroup(bounds);
-    const fronteira = pontos.getBounds();
-    leafletData.getMap('geoLocationMap').then(
-        map => {
-        //  console.log(pontos);
-        //  console.log(fronteira);
-          // pontos.addTo(map);
-          // map.fitBounds(pontos.getBounds())
-          map.fitBounds([
-              [40.712, -74.227],
-              [40.774, -74.125]
-          ]);
-        }
-    )
-      
+          let pracaId = praca.id_pub.split("-")[0];
+          this.geoLoc.markers[pracaId] = {
+            lat: parseFloat(praca.latlong.split(', ')[0]),
+            lng: parseFloat(praca.latlong.split(', ')[1]),
+            message: praca.nome,
+            draggable: false,
+            focus: false
+          }
+          const marker = L.marker(praca.latlong.split(',')[0], praca.latlong.split(',')[1]);
+          bounds.push(marker);
+          if (index === pracas.length - 1){ 
+            leafletData.getMap('geoLocationMap').then(
+              map => map.setView(
+                new L.LatLng(
+                  praca.latlong.split(',')[0],
+                  praca.latlong.split(',')[1]),
+                  9)
+              )
+          }
+        })
+      ).catch(error => console.warn(error))
 
   }
+
 }
 
 export default GeolocCtrl;
