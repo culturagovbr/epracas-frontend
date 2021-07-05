0153ae9879f1385ce636e388c1507838cbec8db9
Ronald Piacenti Junior <rpiacentijr@yahoo.com.br>
2021-07-05 14:56:54 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task("browserify", ["views"], () => {
           context: {
             IDCULTURA_URL:  "https://id.cultura.gov.br",
             IDCULTURA_CLIENTID: "OClientIDdaAplicação",
-            EPRACAS_API_URL: "http://localhost:8000/api/v1",
+            EPRACAS_API_URL: "http://hmg.epraca.cultura.gov.br/api/v1",
           },
         }],
       ],
